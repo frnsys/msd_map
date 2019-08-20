18a9309a2b04f9197fc4511468de8b6ac9cdcc12
Francis Tseng <f+accounts@frnsys.com>
2019-08-20 12:17:14 -0400

    better tile generation, for less gaps at lower zooms

--- data/make_tiles.sh
+++ data/make_tiles.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-tippecanoe -o msd.mbtiles -zg --coalesce-densest-as-needed --extend-zooms-if-still-dropping --generate-ids msd.geojson
+rm msd.mbtiles
+tippecanoe -o msd.mbtiles -zg --coalesce-densest-as-needed --extend-zooms-if-still-dropping --generate-ids --detect-shared-borders --no-line-simplification msd.geojson
