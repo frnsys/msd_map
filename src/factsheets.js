d2b3e2ea0b444d8a5709617bc8be7beb496ef4f0
Francis Tseng <f+accounts@frnsys.com>
2020-10-07 17:11:50 -0400

    factsheet tweaks

--- src/factsheets.js
+++ src/factsheets.js
@@ -7,3 +7,14 @@ mapboxgl.accessToken = config.MAPBOX_TOKEN;
 const maps = {};
 maps['a'] = FSMSDMap(config.CD, 'a');
 maps['b'] = FSMSDMap(config.CD, 'b');
+
+document.getElementById('a--map').addEventListener('mouseleave', () => {
+  document.getElementById('a--map-tooltip').style.display = 'none';
+});
+document.getElementById('b--map').addEventListener('mouseleave', () => {
+  document.getElementById('b--map-tooltip').style.display = 'none';
+});
+
+
+maps['a'].map.resize();
+maps['b'].map.resize();
